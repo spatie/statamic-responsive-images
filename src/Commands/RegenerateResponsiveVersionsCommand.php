dc9aa83f0551438e1a47df3f007159af8e6befa1
riasvdv <riasvdv@users.noreply.github.com>
2020-12-11 13:12:54 +0000

    Fix styling

--- src/Commands/RegenerateResponsiveVersionsCommand.php
+++ src/Commands/RegenerateResponsiveVersionsCommand.php
@@ -2,13 +2,9 @@
 
 namespace Spatie\ResponsiveImages\Commands;
 
-use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
-use Spatie\ResponsiveImages\Jobs\GenerateImageJob;
-use Spatie\ResponsiveImages\WidthCalculator;
 use Statamic\Console\RunsInPlease;
-use Statamic\Contracts\Assets\Asset;
 use Statamic\Contracts\Assets\AssetRepository;
 
 class RegenerateResponsiveVersionsCommand extends Command
@@ -21,7 +17,7 @@ class RegenerateResponsiveVersionsCommand extends Command
 
     public function handle(AssetRepository $assets)
     {
-        if (!config('statamic.assets.image_manipulation.cache')) {
+        if (! config('statamic.assets.image_manipulation.cache')) {
             $this->error('Caching is not enabled for image manipulations, generating them will have no benefit.');
 
             return;
