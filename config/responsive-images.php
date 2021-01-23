ed253b3da2c1b65b87b3afa1a2cee1c79a2a98aa
Michael Aerni <hello@michaelaerni.ch>
2021-01-23 16:54:27 +0100

    Add breakpoint options to config

--- config/responsive-images.php
+++ config/responsive-images.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+
     /**
      * If the generate images job is being queued, specify the name of the target queue
      * Falls back to the 'default' queue
@@ -11,4 +12,21 @@ return [
      * Set a global max width for generated images
      */
     'max_width' => null,
+
+    /*
+     * Define the breakpoints you want to have available to art direct your images
+     */
+    'breakpoints' => [
+        'sm' => 640,
+        'md' => 768,
+        'lg' => 1024,
+        'xl' => 1280,
+        '2xl' => 1536,
+    ],
+
+    /*
+     * The unit of the breakpoints
+     */
+    'breakpoint_unit' => 'px',
+
 ];
