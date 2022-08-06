4d5d1fcec7467d4cbf7867130d9a4dbd4e8e5ef4
ncla <ncla@users.noreply.github.com>
2022-08-06 13:50:45 +0300

    Add replicator test

--- tests/Feature/AssetReferenceTest.php
+++ tests/Feature/AssetReferenceTest.php
@@ -32,6 +32,8 @@ class AssetReferenceTest extends TestCase
         'visibility' => 'visible',
     ];
 
+    private $entryBlueprintWithSingleResponsiveField;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -45,6 +47,15 @@ class AssetReferenceTest extends TestCase
         $this->asset = $this->assetContainer->makeAsset($path)->upload($file);
 
         Stache::clear();
+
+        $this->entryBlueprintWithSingleResponsiveField = [
+            'fields' => [
+                [
+                    'handle' => 'avatar',
+                    'field' => $this->responsiveFieldConfiguration,
+                ]
+            ],
+        ];
     }
 
     protected function setInBlueprints($namespace, $blueprintContents)
@@ -54,25 +65,23 @@ class AssetReferenceTest extends TestCase
         Facades\Blueprint::shouldReceive('in')->with($namespace)->andReturn(collect([$blueprint]));
     }
 
-    protected function createDummyCollectionEntry($data)
+    /**
+     * @param $blueprintConfiguration
+     * @param $entryData
+     * @return \Statamic\Entries\Entry
+     */
+    protected function createDummyCollectionEntry($blueprintConfiguration, $entryData)
     {
         // Create collection
         $collection = tap(Facades\Collection::make('articles'))->save();
 
-        $blueprintContents = [
-            'fields' => [
-                [
-                    'handle' => 'avatar',
-                    'field' => $this->responsiveFieldConfiguration,
-                ]
-            ],
-        ];
+        $blueprintContents = $blueprintConfiguration;
 
         // Create blueprint for collection
         $this->setInBlueprints('collections/articles', $blueprintContents);
 
         // Create entry in the collection
-        return tap(Facades\Entry::make()->collection($collection)->data($data))->save();
+        return tap(Facades\Entry::make()->collection($collection)->data($entryData))->save();
     }
 
     /** @test * */
@@ -118,7 +127,7 @@ class AssetReferenceTest extends TestCase
     /** @test */
     public function asset_reference_gets_updated_after_asset_rename()
     {
-        $entry = $this->createDummyCollectionEntry([
+        $entry = $this->createDummyCollectionEntry($this->entryBlueprintWithSingleResponsiveField, [
             'avatar' => [
                 'src' => 'test_container::test.jpg'
             ],
@@ -134,7 +143,7 @@ class AssetReferenceTest extends TestCase
     /** @test */
     public function asset_array_reference_gets_updated_after_asset_rename()
     {
-        $entry = $this->createDummyCollectionEntry([
+        $entry = $this->createDummyCollectionEntry($this->entryBlueprintWithSingleResponsiveField, [
             'avatar' => [
                 'src' => [
                     'test_container::test.jpg'
@@ -147,5 +156,66 @@ class AssetReferenceTest extends TestCase
         $this->asset->rename('new-test2');
 
         $this->assertEquals(['test_container::new-test2.jpg'], Arr::get($entry->fresh()->get('avatar'), 'src'));
+    }
+
+    /** @test */
+    public function asset_reference_gets_updated_in_replicator_set_after_asset_rename()
+    {
+        $blueprintContents = [
+            'fields' => [
+                [
+                    'handle' => 'test_replicator_field',
+                    'field' => [
+                        'collapse' => false,
+                        'previews' => true,
+                        'sets' => [
+                            'new_test_set' => [
+                                'display' => 'New Test Set',
+                                'fields' => [
+                                    [
+                                        'handle' => 'responsive_test_replicator',
+                                        'field' => $this->responsiveFieldConfiguration,
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'display' => 'Test Replicator Field',
+                        'type' => 'replicator',
+                        'icon' => 'replicator',
+                        'listable' => 'hidden',
+                        'instructions_position' => 'above',
+                        'visibility' => 'visible',
+                    ],
+                ]
+            ]
+        ];
+
+        $entryData = [
+            'test_replicator_field' => [
+                [
+                    'responsive_test_replicator' => [
+                        'src' => [
+                            'test_container::test.jpg'
+                        ],
+                    ],
+                    'type' => 'new_test_set',
+                    'enabled' => true,
+                ],
+            ],
+        ];
+
+        $entry = $this->createDummyCollectionEntry($blueprintContents, $entryData);
+
+        $this->assertEquals(
+            'test_container::test.jpg',
+            Arr::get($entry->get('test_replicator_field'), '0.responsive_test_replicator.src.0')
+        );
+
+        $this->asset->rename('new-test2');
+
+        $this->assertEquals(
+            'test_container::new-test2.jpg',
+            Arr::get($entry->fresh()->get('test_replicator_field'), '0.responsive_test_replicator.src.0')
+        );
     }
 }
