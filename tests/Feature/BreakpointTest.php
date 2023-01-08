73139b391a48495fe4524ae7ddce1bd9823ba766
Lars Heidkämper <10990014+heidkaemper@users.noreply.github.com>
2023-01-08 21:17:15 +0100

    Placeholder generation now fails silently on production by logging exceptions if `APP_DEBUG` is `false` (#189)
    * Account for broken images when generating placeholders
    
    * Throw UnableToReadFile exception right away when in debug mode
    
    * Don't output empty placeholder when a UnableToReadFile exception occurs
    
    * Use the FilesystemException interface from Flysystem
    
    * Use $assetMimeType variable
    
    Co-authored-by: ncla <ncla@users.noreply.github.com>

--- tests/Feature/BreakpointTest.php
+++ tests/Feature/BreakpointTest.php
@@ -122,7 +122,7 @@ it('generates placeholder data url when toggling cache form on to off', function
     /**
      * We use Blink cache for placeholder generation that we need to clear just in case
      * @see https://statamic.dev/extending/blink-cache
-     * @see Breakpoint::placeholderSvg()
+     * @see Breakpoint::placeholder()
      */
     Blink::store()->flush();
 
@@ -141,3 +141,20 @@ it('generates placeholder data url when toggling cache form on to off', function
     expect($secondPlaceholder)->toEqual($firstPlaceholder)
         ->and($cacheDiskPathAfter)->not->toEqual($cacheDiskPathBefore);
 });
+
+it("doesn't crash when the placeholder image cannot be read", function () {
+    $responsive = new Breakpoint($this->asset, 'default', 0, []);
+
+    // Generate placeholder to trigger caching
+    $responsive->placeholder();
+
+    // Forget cached files
+    $pathPrefix = \Statamic\Imaging\ImageGenerator::assetCachePathPrefix($this->asset);
+
+    \Statamic\Facades\Glide::server()->deleteCache($pathPrefix.'/'.$this->asset->path());
+
+    Blink::store()->flush();
+
+    // Generate new placeholder
+    $responsive->placeholder();
+})->expectNotToPerformAssertions();
