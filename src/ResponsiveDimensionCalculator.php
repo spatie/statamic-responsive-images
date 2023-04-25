8e1bee25e90ef1a0c4ead383781018255929010c
Anton Hedling <anton.hedling@inly.se>
2023-04-25 10:35:32 +0200

    Fix config variable

--- src/ResponsiveDimensionCalculator.php
+++ src/ResponsiveDimensionCalculator.php
@@ -77,7 +77,7 @@ class ResponsiveDimensionCalculator implements DimensionCalculator
         $pixelPrice = $predictedFileSize / $area;
 
         while (true) {
-            $predictedFileSize *= config('statamic.responsive-images.predicted_file_size_multiplier', 0.7);
+            $predictedFileSize *= config('statamic.responsive-images.srcset_dimensions_multiplier', 0.7);
 
             $newWidth = (int) floor(sqrt(($predictedFileSize / $pixelPrice) / $ratioForFilesize));
 
